91543f2b90d0f329d32d1408ba1113cacc7bf5af
Rafi Khan <zetoslab@gmail.com>
2017-01-22 01:40:54 -0600

    working on it
    - added utility functions for printing table
    - enabled autoreload of config
    - added modkey

--- init.lua
+++ init.lua
@@ -6,20 +6,95 @@ Licensed under the MIT License
 
  --]]
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
+-- utility functions
+-- Submitted PR (https://github.com/Hammerspoon/hammerspoon/pull/1203) to add this upstream
+function print_r ( t )  
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+ 
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
+-- zwm
 
-  f.x = f.x - 10
-  win:setFrame(f)
-end)
+-- configuration file
+
+config_file_dir = os.getenv("HOME").. "/.zwm"
+
+
+function load_config(file)
+    local config_source = hs.execute("cat " .. config_file_dir .. "/config.json")
+
+    if config_source == "" then
+        hs.alert.show("empty config")
+    else 
+        hs.printf("zwm config: \n" .. config_source)
+        config = hs.json.decode(config_source)
+        hs.alert.show("loaded config")
+    end
+
+end
+
+function reload_config(files)
+   doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-11) == "config.json" then
+            doReload = true
+        end
+    end
+    if doReload then  
+        load_config(config_file_dir)
+    end
+end
+
+load_config(config_file_path)
+
+if config["autoReload"] then
+    local config_watcher = hs.pathwatcher.new(config_file_dir, reload_config):start()
+end
+
+-- key bindings
+if config["meta"] ~= "" then
+    local num = 0
+    mod = {}
+    for word in string.gmatch(config["meta"], '([^-]+)') do
+        mod[num] = word
+        num = num + 1
+    end
+end
+
+-- hammerspoon
 
 -- reload this file on change.
-function reloadConfig(files)
+function reload_hammerspoon(files)
     doReload = false
     for _,file in pairs(files) do
         if file:sub(-4) == ".lua" then
@@ -30,5 +105,5 @@ function reloadConfig(files)
         hs.reload()
     end
 end
-local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Hammerspoon reloaded")
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_hammerspoon):start()
+hs.alert.show("Hammerspoon loaded")
