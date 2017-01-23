9859c9a7b6a6a0cfb7fbcf8afd69bc1a9b95d51b
Rafi Khan <zetoslab@gmail.com>
2017-01-22 22:35:24 -0600

    working on window movement

--- init.lua
+++ init.lua
@@ -6,7 +6,72 @@ Licensed under the MIT License
 
  --]]
 
+-- utility functions
+function error(msg)
+    print("error: \n" .. msg)
+end
+
 -- zwm
+
+
+function load()
+    if config then
+        hs.printf("Config loaded")
+
+        if config["autoReload"] then
+            local config_watcher = hs.pathwatcher.new(config_file_dir, reload_config):start()
+        end
+
+        local mod = config["mod"]
+
+        -- key bindings
+        if config["key_bindings"] then
+
+            for binding, info in pairs(config["key_bindings"]) do
+                
+                if info["type"] == "application" then
+                    if (info["key"] ~= "" and info["action"] ~= "") then 
+                        hs.hotkey.bind(mod, info["key"], nil, function() 
+                            hs.application.open(info["action"])
+                        end)
+                    end
+                elseif info["type"] == "window" then 
+                    if (info["key"] ~= "" and info["action"] ~= "") then 
+                        local function bind()
+                            local win = hs.window.focusedWindow()
+                            local f = win:frame()
+                            local move_amount = config["window_management"]["move_amount"]
+                            
+                            if info["action"] == "window_left" then
+                                f.x = f.x - move_amount
+                            elseif info["action"] == "window_right" then
+                                f.x = f.x + move_amount
+                            elseif info["action"] == "window_up" then
+                            -- BUG: Up is down down is up https://github.com/Hammerspoon/hammerspoon/issues/1208
+                                f.y = f.y - move_amount
+                            elseif info["action"] == "window_down" then
+                                f.y = f.y + move_amount
+                            else
+                                error("invalid window movement binding: " .. info["action"])
+                            end
+
+                            win:setFrame(f)
+                        end
+
+                        hs.hotkey.bind(mod, info["key"], bind, nil, bind)
+                    end
+
+                else
+                    print("error: " .. info["type"] .. " not implemented")
+
+                end
+                
+            end
+        end
+    else
+        hs.alert.show("Config not loaded properly")
+    end
+end
 
 -- configuration file
 
@@ -21,6 +86,7 @@ function load_config(file)
     else 
         hs.printf("zwm config: \n" .. config_source)
         config = hs.json.decode(config_source)
+        load()
         hs.alert.show("loaded config")
     end
 
@@ -39,39 +105,6 @@ function reload_config(files)
 end
 
 load_config(config_file_path)
-
-if config then
-    hs.printf("Config loaded")
-
-    if config["autoReload"] then
-        local config_watcher = hs.pathwatcher.new(config_file_dir, reload_config):start()
-    end
-
-    local mod = config["mod"]
-
-    -- key bindings
-    if config["key_bindings"] then
-
-        for binding, info in pairs(config["key_bindings"]) do
-            
-            if info["type"] == "application" then
-
-                if (info["key"] ~= "" and info["app"] ~= "") then 
-                    hs.hotkey.bind(mod, info["key"], nil, function() 
-                        hs.application.open(info["app"])
-                    end)
-                end
-
-            else
-            print("error: " .. info["type"] .. " not implemented")
-
-            end
-            
-        end
-    end
-else
-    hs.alert.show("Config not loaded properly")
-end
 
 -- hammerspoon
 
