8f0312f2f8f2296f503d0150da92f4e867a21f1b
Rafi Khan <zetoslab@gmail.com>
2017-03-26 01:58:41 -0600

    working on #18

--- menubar.lua
+++ menubar.lua
@@ -7,10 +7,13 @@ menubar = hs.menubar.new()
 function set_workspaces()
 	local all_spaces = get_spaces()
 	local current = spaces.activeSpace()
-	local output = ""
+	output = ""
 	local separator = ""
-	local color = tostring(ansi_color(config["bar"].color))
-	local bg = tostring(ansi_color(config["bar"].bg))
+	local color_active = hex_to_rgb(config["bar"].color_active)
+	local color_inactive = hex_to_rgb(config["bar"].color_inactive)
+
+	start = nil
+	ending = nil
 
 	if config["bar"].separator then
 		separator = config["bar"].separator
@@ -21,18 +24,58 @@ function set_workspaces()
 	for i in ipairs(all_spaces) do
 		t = replace_identifier(tostring(i))
 		if all_spaces[i] == current then
-
-			output = output .. '\27[' .. color .. 'm' .. t .. separator .. '\27[0m'
+			start = output:len()
+			output = output .. t .. separator
+			ending = output:len()
 			
 		else
-			output = output .. '\27[' .. bg .. 'm' .. t .. separator .. '\27[0m'
+			output = output .. t .. separator
 		end
+	end
+
+	if start == 0 then
+		start = 1
 	end
 
 	-- fontawesome
 	local font = {font = {name = "FontAwesome", size = config["bar"].size}}
-	-- local str  = hs.styledtext.ansi(, {font={name="Monaco",size=10},backgroundColor={alpha=1}} )
-	local str = hs.styledtext.ansi(output, font)
+	local active = {starts = start,
+		ends = ending2,
+		attributes = { 
+		color = { 
+			red = color_active.r, 
+			green = color_active.g,
+			blue = color_active.b
+			},
+			font = {name = "FontAwesome", size = config["bar"].size}
+		}
+	}
+
+	local inactive_before = {starts = 1 ,
+		ends= start,
+		attributes = {
+		color = { 
+			red = color_inactive.r, 
+			green = color_inactive.g,
+			blue = color_inactive.b
+			},
+			font = {name = "FontAwesome", size = config["bar"].size}
+		}}
+
+		local inactive_after = {starts = ending,
+		ends= output:len() + 1,
+		attributes = {
+		color = { 
+			red = color_inactive.r, 
+			green = color_inactive.g,
+			blue = color_inactive.b
+			},
+			font = {name = "FontAwesome", size = config["bar"].size}
+		}}
+
+
+	-- local str  F hs.styledtext.ansi(, {font={name="Monaco",size=10},backgroundColor={alpha=1}} )
+	local str = hs.styledtext.new({output, inactive_before, active, inactive_after})
 	menubar:setTitle(str)
 end
 
